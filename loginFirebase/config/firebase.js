b03df8d6192178dddf83bef1b50afd63b884faa8
Hakinori123 <hadanup2@gmail.com>
2026-09-01 14:10:47 +0000

    navigation code

---
+++ loginFirebase/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRFRaEENxuULSoCe22e0dxY3ZYKoFSylQ",
+  authDomain: "login-firebase-e4a3b.firebaseapp.com",
+  projectId: "login-firebase-e4a3b",
+  storageBucket: "login-firebase-e4a3b.firebasestorage.app",
+  messagingSenderId: "398234338549",
+  appId: "1:398234338549:web:f896e330623227c44ef1bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
